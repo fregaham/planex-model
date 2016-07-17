94700278fc1d71c7d15d6e368ea0114f926d71e9
Marek Schmidt <fregaham@gmail.com>
2016-07-17 19:46:05 +0200

    experimental monotonic algorithm

--- src/model.py
+++ src/model.py
@@ -12,9 +12,22 @@ def lcm(a, b):
 def lcmm(*args):
     return reduce(lcm, args)
 
-def _merged_event_interval_monotonic(merged, startTime, endTime, value):
+def bisect_monotonic_interval(merged, lt, lv, rt, rv):
 
-    print `merged`
+    sv = lv - _value_at_time(lt, merged)
+
+    lo = lt
+    hi = rt
+    while lo < hi:
+        mid = (lo+hi)//2
+
+        v = _value_at_time(mid, merged) + sv
+
+        if v >= 0: lo = mid+1
+        else: hi = mid
+    return lo
+
+def _merged_event_interval_monotonic(merged, startTime, endTime, value):
 
     nextdectime = endTime
     nextinctime = endTime
@@ -44,42 +57,39 @@ def _merged_event_interval_monotonic(merged, startTime, endTime, value):
 
     sv = _value_at_time(startTime - 1, merged)
 
+    v = _value_at_time(startTime, merged) - sv + value
+    if v < 0:
+        return (startTime, v, v - value, v - value)
+
     pt = startTime
-    pv = value
+    pv = v
     vmin = value
+    if v < vmin:
+        vmin = v
+
     while t < endTime:
 
-    
-        print "sv == " + str(sv) + " value == " + str(value)
-        v = _value_at_time(t, merged) - sv + value
-
-        print `(t, v)`
-
-        if v < 0:
-            # must have occured somewhere between pt and t
-            print "running interval"
-            return _merged_event_interval(merged, pt, t, _value_at_time(pt - 1, merged) - sv + value)
-
-        if v < vmin:
-            vmin = v
-
-        pv = v
-        pt = t
-
         if inc:
-            nextinctime = endTime
-            for x in merged:
-                if x[2] > 0:
-                    diff = (x[0] - (((t + 1 - x[1]) % x[0])))
-                    if diff == x[0]:
-                        xnt = t + 1
-                    else:
-                        xnt = t + 1 + diff
-                    if xnt < nextinctime:
-                        nextinctime = xnt
-            t = nextdectime
-            inc = False
-        else:
+
+            if t > pt + 1:
+                v = _value_at_time(t - 1, merged) - sv + value
+                if v < vmin:
+                    vmin = v
+                if v < 0:
+                    t = bisect_monotonic_interval(merged, pt, pv, t - 1, v)
+                    v = _value_at_time(t, merged) - sv + value
+                    return (t, v, v - value, vmin - value)
+                pt = t - 1
+                pv = v
+            
+            v = _value_at_time(t, merged) - sv + value
+            if v < vmin:
+                vmin = v
+            if v < 0:
+                return (t, v, v - value, vmin - value)
+            pt = t
+            pv = v
+
             nextdectime = endTime
             for x in merged:
                 if x[2] < 0:
@@ -90,9 +100,42 @@ def _merged_event_interval_monotonic(merged, startTime, endTime, value):
                         xnt = t + 1 + diff
                     if xnt < nextdectime:
                         nextdectime = xnt
+            t = nextdectime
+
+            inc = False
+        else:
+
+            if t > pt + 1:
+                v = _value_at_time(t - 1, merged) - sv + value
+                if v < vmin:
+                    vmin = v
+                if v < 0:
+                    t = bisect_monotonic_interval(merged, pt, pv, t - 1, v)
+                    v = _value_at_time(t, merged) - sv + value
+                    return (t, v, v - value, vmin - value)
+                pt = t - 1
+                pv = v
+ 
+            v = _value_at_time(t, merged) - sv + value
+            if v < vmin:
+                vmin = v
+            if v < 0:
+                return (t, v, v - value, vmin - value)
+            pt = t
+            pv = v
+
+            nextinctime = endTime
+            for x in merged:
+                if x[2] > 0:
+                    diff = (x[0] - (((t + 1 - x[1]) % x[0])))
+                    if diff == x[0]:
+                        xnt = t + 1
+                    else:
+                        xnt = t + 1 + diff
+                    if xnt < nextinctime:
+                        nextinctime = xnt
             t = nextinctime
             inc = True
-
 
     diff = _value_at_time(endTime - 1, merged) - sv
 
@@ -101,15 +144,28 @@ def _merged_event_interval_monotonic(merged, startTime, endTime, value):
         vmin = v
 
     if v < 0:
-        # must have occured somewhere between pt and t
-        print "running interval"
-        return _merged_event_interval(merged, pt, t, _value_at_time(pt - 1, merged) - sv + value)
-
-    print `(endTime -1, v)`
+        t = endTime - 1
+        t = bisect_monotonic_interval(merged, pt, pv, t, v)
+        v = _value_at_time(t, merged) - sv + value
+        return (t, v, v - value, vmin - value)
 
     ret = (endTime, v, diff, vmin - value)
-    print "XXX " + `ret`
     return ret
+
+def _merged_event_interval_slow(merged, startTime, endTime, value):
+    dv = value - _value_at_time(startTime - 1, merged)
+
+    vmin = value
+    diff = 0    
+
+    for i in xrange(startTime, endTime):
+        v = dv + _value_at_time(i, merged)
+        if v < vmin:
+            vmin = v
+        if v < 0:
+            return (i, v, v - value, vmin - value)
+    
+    return (endTime, v, v - value, vmin - value)
 
 def _merged_event_interval(merged, startTime, endTime, value):
 
@@ -127,7 +183,6 @@ def _merged_event_interval(merged, startTime, endTime, value):
         else:
             xnt = t + diff
 
-        #xnt = _nextevent(t, merged[i])
         nexttimes[i] = xnt
         if xnt < nt:
             nt = xnt
@@ -138,7 +193,6 @@ def _merged_event_interval(merged, startTime, endTime, value):
     _dmin = 0
 
     while t < endTime:
-
         nt = endTime
         for i in xrange(l):
             xnt = nexttimes[i]
@@ -164,8 +218,6 @@ def _merged_event_interval(merged, startTime, endTime, value):
         assert t < nt
 
         t = nt
-
-    print "YYY " + `(t, value, diff, _dmin)`
 
     return (t, value, diff, _dmin)
 
@@ -204,24 +256,35 @@ def times_of_under_and_overflows(startTime, initialValue, minValue, maxValue, fl
     
     underflow = _time_of_underflow(startTime, initialValue - minValue, l, merged)
     
-#    merged_negative = map(lambda x:(x[0],x[1],-x[2]), merged)
+    merged_negative = map(lambda x:(x[0],x[1],-x[2]), merged)
 
-#    overflow = _time_of_underflow(startTime, maxValue - initialValue, l, merged_negative)
+    overflow = _time_of_underflow(startTime, maxValue - initialValue, l, merged_negative)
 
-    return (underflow, None)
-#    return (underflow, overflow)
+#    return (underflow, None)
+    return (underflow, overflow)
 
 def times_of_under_and_overflows_monotonic(startTime, initialValue, minValue, maxValue, flows):
     l, merged =_merge_flows(flows)
     
     underflow = _time_of_underflow_monotonic(startTime, initialValue - minValue, l, merged)
     
-#    merged_negative = map(lambda x:(x[0],x[1],-x[2]), merged)
+    merged_negative = map(lambda x:(x[0],x[1],-x[2]), merged)
 
-#    overflow = _time_of_underflow_monotonic(startTime, maxValue - initialValue, l, merged_negative)
+    overflow = _time_of_underflow_monotonic(startTime, maxValue - initialValue, l, merged_negative)
 
-    return (underflow, None)
-#    return (underflow, overflow)
+    return (underflow, overflow)
+
+def times_of_under_and_overflows_slow(startTime, initialValue, minValue, maxValue, flows):
+    l, merged =_merge_flows(flows)
+    
+    underflow = _time_of_underflow_slow(startTime, initialValue - minValue, l, merged)
+    
+    merged_negative = map(lambda x:(x[0],x[1],-x[2]), merged)
+
+    overflow = _time_of_underflow_slow(startTime, maxValue - initialValue, l, merged_negative)
+
+    return (underflow, overflow)
+
 
 def _time_of_underflow(starttime, initialvalue, l, merged):
 
@@ -252,11 +315,37 @@ def _time_of_underflow(starttime, initialvalue, l, merged):
 
     assert false
 
-def _time_of_underflow_monotonic(starttime, initialvalue, l, merged):
+def _time_of_underflow_slow(starttime, initialvalue, l, merged):
 
-    print `merged`
-    for i in xrange(300):
-        print "value at %d: %d" % (i, _value_at_time(i, merged) - _value_at_time(starttime - 1, merged) + initialvalue )
+    t = starttime
+
+    diff = 0
+    _min = 0
+    _max = 0
+
+    value = initialvalue
+
+    t, tval, diff, _min = _merged_event_interval_slow(merged, starttime, starttime + l, value)
+    if tval < 0:
+        return t
+
+    if diff >= 0:
+        return None
+       
+    iterations = 1 + (initialvalue + _min) // - diff
+
+    laststarttime = starttime + iterations * l
+    value = initialvalue + iterations * diff
+
+    t, tval, diff, _min = _merged_event_interval_slow(merged, laststarttime, laststarttime + l, value)
+
+    if tval < 0:
+        return t
+
+    assert False
+
+
+def _time_of_underflow_monotonic(starttime, initialvalue, l, merged):
 
     t = starttime
 
@@ -278,42 +367,38 @@ def _time_of_underflow_monotonic(starttime, initialvalue, l, merged):
     laststarttime = starttime + iterations * l
     value = initialvalue + iterations * diff
 
-    print "value at start of laststartime: %d" % value
-    print "real value at laststarttime (%d) -1: %d" % (laststarttime,  _value_at_time(laststarttime - 1, merged) - _value_at_time(starttime - 1, merged) + initialvalue)
-    print "real value at laststarttime (%d)   : %d" % (laststarttime, _value_at_time(laststarttime, merged) - _value_at_time(starttime - 1, merged) + initialvalue)
-
-
     t, tval, diff, _min = _merged_event_interval_monotonic(merged, laststarttime, laststarttime + l, value)
 
     if tval < 0:
         return t
 
-    assert false
+    assert False
 
 
 
 def _value_at_time(time, merged):
     v = 0
     for period, phase, amount in merged:
-        #v = (time + phase + 1) // period  - (startTime + phase + 1) // period
-        v += amount * ((time + phase) // period)
+        v += amount * ((time - phase) // period)
 
     return v
 
 
 if __name__ == "__main__":
 
-    i = 100
-    j = 0
-#    for i in xrange(0, 100):
-#        for j in xrange(0, 300):
-    u, o = times_of_under_and_overflows (j, i, 0, 110, [(10,0,1), (10,0,-1), (100,0,5), (100,0,-4), (100,0,-5), (10,0,1), (10, 0, -5)])
-    um, om = times_of_under_and_overflows_monotonic (j, i, 0, 110, [(10,0,1), (10,0,-1), (100,0,5), (100,0,-4), (100,0,-5), (10,0,1), (10, 0, -5)])
+    i = 10
+    j = 112
+    flows = [(500, 0, 100), (600, 0, 50), (1,0,-1)]
 
-    print `(u, o, um, om)`
-    assert u == um and o == om
+    for i in xrange(0, 100):
+        for j in xrange(0, 300):
+#             print `(i, j)`
+             um, om = times_of_under_and_overflows_monotonic (j, i, 0, 107, flows)
+             u, o = times_of_under_and_overflows (j, i, 0, 107, flows)
 
-    
-#    for i in xrange(0, 10):
-#        _diff_at_time(2, i, 5, [(5, 2, 1)])
+             print `(u, o, um, om)`
+
+#             print `(um, om)`
+             assert u == um
+             assert o == om
 
